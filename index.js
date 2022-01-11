d716030f0ecfe28cb3d4497ace77a1299d1b03e5
woosuk-choi0523 <61082292+woosuk-choi0523@users.noreply.github.com>
2022-01-11 13:23:26 +0900

    Updata jquary & edit some detail

---
+++ index.js
@@ -0,0 +1,69 @@
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    $(".login-cover").hide();
+	$("#signOutBtn").show();
+    var dialog = document.querySelector('#loginDialog');
+    /*
+    if (! dialog.showModal) {
+      dialogPolyfill.registerDialog(dialog);
+    }
+    */
+    dialog.close();
+
+  } else {
+
+    $(".login-cover").show();
+	$("#signOutBtn").hide();
+
+    // No user is signed in.
+    var dialog = document.querySelector('#loginDialog');
+    if (! dialog.showModal) {
+      dialogPolyfill.registerDialog(dialog);
+    }
+    dialog.close();
+
+  }
+});
+
+
+/* LOGIN PROCESS */
+$("#loginBtn").click(
+  function(){
+
+
+    var email = $("#loginEmail").val();
+    var password = $("#loginPassword").val();
+
+    if(email != "" && password != ""){
+      $("#loginProgress").show();
+      $("#loginBtn").hide();
+
+      firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        $("#loginError").show().text(errorMessage);
+        $("#loginProgress").hide();
+        $("#loginBtn").show();
+      });
+    }
+  }
+);
+
+/* LOGOUT PROCESS */
+
+$("#signOutBtn").click(
+  function(){
+
+    firebase.auth().signOut().then(function() {
+      // Sign-out successful.
+		$("#loginBtn").show();
+    }).catch(function(error) {
+      // An error happened.
+      alert(error.message);
+    });
+
+  }
+);
